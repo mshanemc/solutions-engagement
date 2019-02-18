1c341a3098871f68cb440cf8c3db8c11c5c510d2
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-18 09:21:42 -0600

    full install in orgInit

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 # cadalys concierge
-# sfdx force:package:install --package  04t460000027gmI -w 50 -r
-# sfdx shane:github:package:install -g mshanemc -r lwc-org-chart
+sfdx force:package:install --package 04t460000027gmI -w 50 -r
+sfdx shane:github:package:install -g mshanemc -r lwc-org-chart
 sfdx force:source:push
 sfdx force:data:tree:import -f data/CollaborationGroup.json
 sfdx force:org:open
