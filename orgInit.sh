baaf813c25057c21935e5c916839a4d07b25d9ac
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-18 10:39:51 -0600

    apps and tabs

--- orgInit.sh
+++ orgInit.sh
@@ -3,5 +3,7 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:package:install --package 04t460000027gmI -w 50 -r
 sfdx shane:github:package:install -g mshanemc -r lwc-org-chart
 sfdx force:source:push
+sfdx force:user:permset:assign -n solutions
+sfdx force:apex:execute -f scripts/connectedAppSetup.cls
 sfdx force:data:tree:import -f data/CollaborationGroup.json
 sfdx force:org:open
