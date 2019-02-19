e65b87be26bc90f82c7736793448b66feff1d132
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-19 11:36:05 -0800

    add safety package stuff

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,8 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+
 # cadalys concierge
-# sfdx force:package:install --package 04t460000027gmI -w 50 -r
-# sfdx shane:github:package:install -g mshanemc -r lwc-org-chart
+sfdx force:package:install --package 04t460000027gmI -w 50 -r
+sfdx shane:github:package:install -g mshanemc -r lwc-org-chart
 sfdx force:source:push
 sfdx force:user:permset:assign -n solutions
 sfdx force:apex:execute -f scripts/connectedAppSetup.cls
@@ -14,10 +15,9 @@ sfdx shane:data:file:upload -f assets/flowchart.pdf
 sfdx shane:data:file:upload -f assets/desk.jpg
 sfdx force:apex:execute -f scripts/chatterSetup.cls
 
-# TODO: install safety package
-# sfdx shane:github:package:install -g mshanemc -r safety
-# sfdx force:user:permset:assign -n Safety
-# sfdx force:data:bulk:upsert -f data/incident_reports.csv -s Incident_Report__c -i id
-# sfdx force:data:bulk:upsert -f data/observations.csv -s Observation__c -i id
+sfdx shane:github:package:install -g mshanemc -r employee-safety
+sfdx force:user:permset:assign -n Safety
+sfdx force:data:bulk:upsert -f data/incident_reports.csv -s Incident_Report__c -i id
+sfdx force:data:bulk:upsert -f data/observations.csv -s Observation__c -i id
 
 sfdx force:org:open -p /lightning/setup/ThemingAndBranding/home
