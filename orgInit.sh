0a3fb05838b9d0ba184371b99fedcda5a60fc613
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-19 16:10:54 -0800

    moar scripting

--- orgInit.sh
+++ orgInit.sh
@@ -2,7 +2,18 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 
 # cadalys concierge
 sfdx force:package:install --package 04t460000027gmI -w 50 -r
+# TODO: create some past tickets via bulk load
+
 sfdx shane:github:package:install -g mshanemc -r lwc-org-chart
+sfdx force:user:create Firstname=Report Lastname=One IsActive=false Title=Designer MobilePhone=8324728021
+sfdx force:user:create Firstname=Report Lastname=Two IsActive=false Title=Designer MobilePhone=8324728021
+sfdx force:user:create Firstname=Report Lastname=Three IsActive=false Title=Engineer MobilePhone=8324728021
+sfdx force:user:create Firstname=Report Lastname=Four IsActive=false Title=Engineer MobilePhone=8324728021
+sfdx force:user:create Firstname=Report Lastname=Five IsActive=false Title=Engineer MobilePhone=8324728021
+sfdx force:user:create Firstname=Sir Lastname=Boss IsActive=false Title=CEO MobilePhone=8324728021
+
+sfdx force:apex:execute -f scripts/orgChartSetup.cls
+
 sfdx force:source:push
 sfdx force:user:permset:assign -n solutions
 sfdx force:apex:execute -f scripts/connectedAppSetup.cls
@@ -16,8 +27,13 @@ sfdx shane:data:file:upload -f assets/desk.jpg
 sfdx force:apex:execute -f scripts/chatterSetup.cls
 
 sfdx shane:github:package:install -g mshanemc -r employee-safety
+sfdx shane:github:package:install -g mshanemc -r electron-theme
 sfdx force:user:permset:assign -n Safety
 sfdx force:data:bulk:upsert -f data/incident_reports.csv -s Incident_Report__c -i id
 sfdx force:data:bulk:upsert -f data/observations.csv -s Observation__c -i id
 
 sfdx force:org:open -p /lightning/setup/ThemingAndBranding/home
+sfdx shane:user:password:set -g Jenny -l Nunez -p sfdx1234
+
+# TODO: set the photos properly instead of my evil version
+sfdx shane:user:allPhotos
