bd03fe422dd92da4e21a04432466bbf86e28876d
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-20 18:16:13 -0800

    and publish the articles, modify concierge

--- orgInit.sh
+++ orgInit.sh
@@ -1,9 +1,10 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -n --userprefix electron --userdomain ee.demo
+sfdx force:data:record:update -s User -w "Name='User User'" -v "FirstName='Jenny' LastName='Nunez' UserPermissionsKnowledgeUser=true"
 sfdx force:data:bulk:upsert -f data/Knowledge__kav.csv -i id -s Knowledge__kav 
 
-# cadalys concierge
+# cadalys concierge takes a while, so bulk api should be reliably finished.  probably.  hopefully.
 sfdx force:package:install --package 04t460000027gmI -w 50 -r
-# TODO: create some past tickets via bulk load
+sfdx force:apex:execute -f scripts/conciergeSetup.cls
 
 sfdx shane:github:package:install -g mshanemc -r lwc-org-chart
 sfdx force:user:permset:assign -n OrgChart
@@ -18,7 +19,6 @@ sfdx force:apex:execute -f scripts/orgChartSetup.cls
 sfdx force:source:push
 sfdx force:user:permset:assign -n solutions
 sfdx force:apex:execute -f scripts/connectedAppSetup.cls
-sfdx force:data:record:update -s User -w "Name='User User'" -v "FirstName='Jenny' LastName='Nunez' UserPermissionsKnowledgeUser=true"
 sfdx force:data:record:update -s User -w "Name='Integration User'" -v "FirstName='William' LastName='Loman'"
 sfdx force:data:record:update -s User -w "Name='Security User'" -v "FirstName='Jesse' LastName='Sanchez'"
 sfdx force:data:tree:import -f data/CollaborationGroup.json
@@ -32,6 +32,8 @@ sfdx shane:github:package:install -g mshanemc -r electron-theme
 sfdx force:user:permset:assign -n Safety
 sfdx force:data:bulk:upsert -f data/incident_reports.csv -s Incident_Report__c -i id
 sfdx force:data:bulk:upsert -f data/observations.csv -s Observation__c -i id
+
+
 
 sfdx force:org:open -p /lightning/setup/ThemingAndBranding/home
 sfdx shane:user:password:set -g Jenny -l Nunez -p sfdx1234
