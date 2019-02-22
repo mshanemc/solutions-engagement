c1ddd31abf25c02988b16d0a23e151c254f9fbbf
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-22 09:34:24 -0800

    height adjustability on branding banner

--- force-app/main/default/lwc/imageBanner/imageBanner.js
+++ force-app/main/default/lwc/imageBanner/imageBanner.js
@@ -6,9 +6,10 @@ export default class ImageBanner extends LightningElement {
     @api backgroundImage;
     @api backgroundColor = 'white';
     @api maxHeight = '150px;'
+    @api minHeight = '150px;'
     @api textColor;
 
     get mainStyle() {
-        return `background-color:${this.backgroundColor};background-image:url(${this.backgroundImage}); max-height:${this.maxHeight};color:${this.textColor}`;
+        return `background-color:${this.backgroundColor};background-image:url(${this.backgroundImage}); max-height:${this.maxHeight};min-height:${this.minHeight};color:${this.textColor}`;
     }
 }
